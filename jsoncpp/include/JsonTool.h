bf20f955db02816f536e212c56a0cecc30ebe4d2
comiee <1440667228@qq.com>
2023-09-04 01:01:37 +0800

    JsonTool增加方法toObject和toArray

--- jsoncpp/include/JsonTool.h
+++ jsoncpp/include/JsonTool.h
@@ -11,16 +11,26 @@ public:
     static Json::Value parse(const string &str);
 
     template<typename K, typename V, typename...Args>
-    inline static Json::Value createObject(K k, V v, Args...args) { return _createObject(k, v, args...); }
+    inline static Json::Value createObject(const K &k, const V &v, Args...args) { return _createObject(k, v, args...); }
 
     template<typename X, typename...Args>
-    inline static Json::Value createArray(X x, Args...args) { return _createArray(x, args...); }
+    inline static Json::Value createArray(const X &x, Args...args) { return _createArray(x, args...); }
+
+    template<typename M>
+    inline static Json::Value toObject(const M &m) { return _toObject(m); }
+
+    template<typename A>
+    inline static Json::Value toArray(const A &a) { return _toArray(a); }
+
+    template<typename It>
+    inline static Json::Value toArray(It begin, It end) { return _toArray(begin, end); }
+
 
 private:
     // 为保持简洁，使用的模板的方法实现放在这里，public里只透传
 
     template<typename K, typename V, typename...Args>
-    static Json::Value _createObject(K k, V v, Args...args) {
+    static Json::Value _createObject(const K &k, const V &v, Args...args) {
         Json::Value root;
         root[k] = v;
         if constexpr (sizeof...(args) > 0) {
@@ -33,7 +43,7 @@ private:
     }
 
     template<typename X, typename...Args>
-    static Json::Value _createArray(X x, Args...args) {
+    static Json::Value _createArray(const X &x, Args...args) {
         Json::Value root;
         root.append(x);
         if constexpr (sizeof...(args) > 0) {
@@ -41,6 +51,33 @@ private:
             for (const Json::Value &item: other) {
                 root.append(item);
             }
+        }
+        return root;
+    }
+
+    template<typename M>
+    static Json::Value _toObject(const M &m) {
+        Json::Value root;
+        for (auto &[k, v]: m) {
+            root[string(k)] = v;
+        }
+        return root;
+    }
+
+    template<typename A>
+    static Json::Value _toArray(const A &a) {
+        Json::Value root;
+        for (auto &x: a) {
+            root.append(x);
+        }
+        return root;
+    }
+
+    template<typename It>
+    static Json::Value _toArray(It begin, It end) { //为了传数组的时候更方便，不使用const&
+        Json::Value root;
+        for (It it = begin; it != end; ++it) {
+            root.append(*it);
         }
         return root;
     }
--- test/test_json.cpp
+++ test/test_json.cpp
@@ -1,5 +1,7 @@
 #include "JsonTool.h"
 #include <iostream>
+#include <map>
+#include <vector>
 
 using namespace std;
 
@@ -12,5 +14,21 @@ void testJsonMain() {
 
     auto b = JsonTool::createObject("a", 1);
     cout << b.type() << JsonTool::dump(b) << endl;
+
+    map<string, int> c{{"1", 3},
+                       {"2", 4}};
+    auto d = JsonTool::toObject(c);
+    cout << JsonTool::dump(d) << endl;
+
+    vector<int> e{5, 6, 7};
+    auto f = JsonTool::toArray(e);
+    cout << JsonTool::dump(f) << endl;
+
+    int g[]{8, 9, 0};
+    auto h = JsonTool::toArray(g);
+    cout << JsonTool::dump(h) << endl;
+
+    auto i = JsonTool::toArray(g, g + 3);
+    cout << JsonTool::dump(i) << endl;
 }
 
